b55dc4b69943be2a9ebaa280e3a48b101e172e44
RJ Salamanca <salamancaronito@gmail.com>
2019-05-28 14:21:35 -0400

    worked with req and res

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.get('/',(req,res)=>{
     res.send(`hello world ${firstName} ${lastName}`).end();
 });
 
-app.get('/name', (req,res)=>{
+app.get('/cats', (req,res)=>{
+    res.send('meow').end();
+});
+
+app.get('/dogs', (req,res)=>{
+    res.send('dogs').end();
+});
+
+app.get('/cats_and_dogs', (req,res)=>{
+    res.send('living together').end();
+});
+
+app.get('/greet/:name', (req,res)=>{
+    const name = req.params.name;
+    res.send('Hello, ' + name).end();
+});
+
+app.get('/hello1', function(req, res){
+
     console.log(req.query)
+    var year = 2019 - req.query.age;
+
+    res.send('You were born in  ' + year + '.');
 });
